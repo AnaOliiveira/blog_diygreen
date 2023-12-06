7d4b9b7a7ef3937e7f5c835f656656e4c899656c
AnaOliiveira <oliveira.miranda2102@gmail.com>
2023-12-05 23:06:16 -0300

    atualizando

---
+++ js/modelo.js
@@ -0,0 +1,42 @@
+let modeloJson = [
+    {
+        id: 1,
+        name: 'Tartaruga com garrafa pet',
+        img: 'img/tartaruga.png',
+        link: 'https://www.youtube.com/watch?v=lcmXECDOZOw',
+      },
+      {
+        id: 2,
+        name: 'Flor com rolo de papel',
+        img: 'img/flor.png',
+        link: 'https://www.youtube.com/watch?v=sExZdQ9T4LE',
+      },
+      {
+        id: 3,
+        name: 'Guardiões da galáxia 3',
+        img: 'img/guar.png',
+        link: 'https://www.youtube.com/watch?v=d1yNc9skssk',
+      },
+
+      {
+        id: 4,
+        name: 'A pequena sereia',
+        img: 'img/sereia.png',
+        link: 'https://www.youtube.com/watch?v=_ma8MMmx72A',
+      },
+
+      {
+        id: 4,
+        name: 'Velozes & Furiosos 10',
+        img: 'img/velozes.png',
+        link: 'https://www.youtube.com/watch?v=F2-BLKAidao',
+      },
+
+      {
+        id: 5,
+        name: 'O parque dos sonhos',
+        img: 'img/parque.png',
+        link: 'https://www.youtube.com/watch?v=zy_AFGqkHVk',
+      },
+
+]
